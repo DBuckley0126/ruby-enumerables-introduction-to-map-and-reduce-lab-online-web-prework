d03051e4f3cda0ca29499cfbcb3ccf44798d3df5
Danny Buckley <dannyboy.msn@hotmail.com>
2019-09-03 18:14:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -80,7 +80,7 @@ def reduce_to_total(source_array, starting_point = 0)
 p total
 end
 
-def reduce_to_total(source_array, starting_point = 0)
+def reduce_to_all_true(source_array))
   
   i = 0
   new_array = []
