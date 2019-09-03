fb2a9867f8f0806c155501aff2cc5e8990f6f29f
Danny Buckley <dannyboy.msn@hotmail.com>
2019-09-03 17:57:49 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -29,3 +29,18 @@ def map_to_no_change(source_array)
 
 p new_array
 end
+
+def map_to_negativize(source_array)
+  
+  i = 0
+  new_array = []
+  
+  while i < source_array.length do
+    
+  new_array << source_array[i] * -1
+  i += 1
+  
+  end
+  
+p new_array
+end
